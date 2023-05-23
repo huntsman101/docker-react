f5d3a761ab8d157a56ba01d728d9e03b8e28e3e3
huntsman101 <seanhunt101@gmail.com>
2023-05-23 23:25:56 +1000

    changed message to good night team 4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Good night Et       </p>
+          Good night team 4       </p>
         <a
           className="App-link"
           href="https://reactjs.org"

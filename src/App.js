5c7966c155ab95d0892c19e2dd9d2f7a8c81f63b
huntsman101 <seanhunt101@gmail.com>
2023-05-24 20:24:46 +1000

    changed message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Good evening team 4       </p>
+          Good evening       </p>
         <a
           className="App-link"
           href="https://reactjs.org"
